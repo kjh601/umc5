a9e7d021b55bdd90fe0e69cbbe4ad660a2d6c647
KJH <67515586+potatoholic@users.noreply.github.com>
2023-11-26 16:38:31 +0900

    feat(ErrorReasonDTO): ErrorReasonDTO 구성
    - API 실패 응답과 과련된 상태 정보를 관리

--- src/main/java/umc/spring/apiPayload/code/ErrorReasonDTO.java
+++ src/main/java/umc/spring/apiPayload/code/ErrorReasonDTO.java
@@ -1,2 +1,14 @@
-package umc.spring.apiPayload.code;public class ErrorReasonDTO {
+package umc.spring.apiPayload.code;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Builder
+public class ErrorReasonDTO {
+    private String message;
+    private String code;
+    private Boolean isSuccess;
+    private HttpStatus httpStatus;
 }
