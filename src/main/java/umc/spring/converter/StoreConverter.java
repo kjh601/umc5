80f07e109ec384b737f7e95aea4dd58733f4e439
KJH <fighter0628@gmail.com>
2023-12-03 14:23:24 +0900

    Feat(StoreConverter): toReviewPreviewListDTO, tpReviewPreviewDTO 구성

--- src/main/java/umc/spring/converter/StoreConverter.java
+++ src/main/java/umc/spring/converter/StoreConverter.java
@@ -1,9 +1,13 @@
 package umc.spring.converter;
 
 import org.locationtech.jts.geom.Point;
+import org.springframework.data.domain.Page;
 import umc.spring.domain.*;
 import umc.spring.web.dto.StoreRequestDTO;
 import umc.spring.web.dto.StoreResponseDTO;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class StoreConverter {
     public static Store toStore(StoreRequestDTO.JoinDTO request, Point point, FoodCategory foodCategory, ZipCode zipCode){
@@ -39,6 +43,30 @@ public class StoreConverter {
         return StoreResponseDTO.ReviewResultDTO.builder()
                 .reviewId(review.getReviewId())
                 .createAt(review.getCreateAt())
+                .build();
+    }
+
+    public static StoreResponseDTO.ReviewPreviewDTO toReviewPreviewDTO(Review review){
+        return StoreResponseDTO.ReviewPreviewDTO.builder()
+                .nickname(review.getUser().getNickname())
+                .rating(review.getRating())
+                .createAt(review.getCreateAt())
+                .contents(review.getContents())
+                .build();
+    }
+
+    public static StoreResponseDTO.ReviewPreviewListDTO toReviewPreviewListDTO(Page<Review> reviewList) {
+
+        List<StoreResponseDTO.ReviewPreviewDTO> reviewPreviewListDTOList = reviewList.stream()
+                .map(StoreConverter::toReviewPreviewDTO)
+                .collect(Collectors.toList());
+        return StoreResponseDTO.ReviewPreviewListDTO.builder()
+                .isLast(reviewList.isLast())
+                .isFirst(reviewList.isFirst())
+                .totalPage(reviewList.getTotalPages())
+                .listSize(reviewList.getSize())
+                .reviewList(reviewPreviewListDTOList)
+                .totalElements(reviewList.getTotalElements())
                 .build();
     }
 }
