d09b23898adf2dc18b5986e15eb144ffc8362492
KJH <fighter0628@gmail.com>
2023-12-03 16:51:11 +0900

    Feat(StoreQueryService): getReviewList 정의 및 구현

--- src/main/java/umc/spring/service/StoreService/StoreQueryServiceImpl.java
+++ src/main/java/umc/spring/service/StoreService/StoreQueryServiceImpl.java
@@ -6,8 +6,10 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import umc.spring.base.apiPayload.code.status.ErrorStatus;
 import umc.spring.base.apiPayload.exception.GeneralException;
+import umc.spring.domain.Mission;
 import umc.spring.domain.Review;
 import umc.spring.domain.Store;
+import umc.spring.repository.MissionRepository;
 import umc.spring.repository.ReviewRepository;
 import umc.spring.repository.StoreRepository;
 
@@ -17,12 +19,21 @@ public class StoreQueryServiceImpl implements StoreQueryService {
 
     private final StoreRepository storeRepository;
     private final ReviewRepository reviewRepository;
+    private final MissionRepository missionRepository;
 
     @Override
     public Page<Review> getReviewList(Long storeId, Integer page) {
         Store store = storeRepository.findById(storeId).orElseThrow(() -> new GeneralException(ErrorStatus.STORE_NOT_FOUND));
 
-        Page<Review> StorePage = reviewRepository.findAllByStore(store, PageRequest.of(page, 10));
-        return StorePage;
+        Page<Review> reviewPage = reviewRepository.findAllByStore(store, PageRequest.of(page, 10));
+        return reviewPage;
+    }
+
+    @Override
+    public Page<Mission> getMissionPreviewList(Long storeId, Integer page) {
+        Store store = storeRepository.findById(storeId).orElseThrow(() -> new GeneralException(ErrorStatus.STORE_NOT_FOUND));
+
+        Page<Mission> missionPage = missionRepository.findAllByStore(store, PageRequest.of(page, 10));
+        return missionPage;
     }
 }
