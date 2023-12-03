a122363251543010dc6a5d091b530f63238a03c7
KJH <fighter0628@gmail.com>
2023-12-03 21:34:01 +0900

    refactor(UserRestController): 특정 유저가 진행 중인 미션 목록 조회 구현

--- src/main/java/umc/spring/web/controller/UserRestController.java
+++ src/main/java/umc/spring/web/controller/UserRestController.java
@@ -10,13 +10,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 import umc.spring.base.apiPayload.ApiResponse;
+import umc.spring.converter.MissionConverter;
 import umc.spring.converter.ReviewConverter;
 import umc.spring.converter.UserConverter;
+import umc.spring.domain.Mission;
 import umc.spring.domain.Review;
 import umc.spring.domain.User;
 import umc.spring.service.UserService.UserCommandService;
 import umc.spring.service.UserService.UserQueryService;
 import umc.spring.validation.annotation.CheckPage;
+import umc.spring.web.dto.MissionResponseDTO;
 import umc.spring.web.dto.ReviewResponseDTO;
 import umc.spring.web.dto.UserRequestDTO;
 import umc.spring.web.dto.UserResponseDTO;
@@ -54,5 +57,24 @@ public class UserRestController {
 
         Page<Review> reviewPage = userQueryService.getReviewList(userId, page);
         return ApiResponse.onSuccess(ReviewConverter.toReviewPreviewListDTO(reviewPage));
+    }
+
+    @GetMapping("/{userId}/missions")
+    @Operation(summary = "특정 유저가 진행 중인 미션 목록 조회 API", description = "특정 유저가 진행 중인 미션들의 목록을 조회하는 API이며, 페이징을 포함합니다. queryString으로 page 번호를 주세요")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003",description = "access 토큰을 주세요!", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH004",description = "access 토큰 만료", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH006",description = "access 토큰 모양이 이상함", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+    })
+    @Parameters({
+            @Parameter(name = "userId", description = "유저의 아이디, path variable 입니다!"),
+            @Parameter(name = "page", description = "페이지 번호, 1번 부터 시작합니다.")
+    })
+    public ApiResponse<MissionResponseDTO.MissionPreviewListDTO> getMissionList(
+            @PathVariable(name = "userId") Long userId,
+            @RequestParam(name = "page") @CheckPage Integer page){
+        Page<Mission> missionPage = userQueryService.getMissionList(userId, page);
+        return ApiResponse.onSuccess(MissionConverter.toMissionPreviewListDTO(missionPage));
     }
 }
