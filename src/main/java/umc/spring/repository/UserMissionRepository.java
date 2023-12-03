248861e5686a28f27d3a7a28f33d3a5868703683
KJH <fighter0628@gmail.com>
2023-12-03 21:38:16 +0900

    feat(UserMissionRepository):findAllByUserAndMissionStatus 정의
    - 특정 유저와 미션상태에 해당하는 UserMission 찾는 기능 정의

--- src/main/java/umc/spring/repository/UserMissionRepository.java
+++ src/main/java/umc/spring/repository/UserMissionRepository.java
@@ -1,7 +1,13 @@
 package umc.spring.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import umc.spring.domain.User;
+import umc.spring.domain.enums.MissionStatus;
 import umc.spring.domain.mapping.UserMission;
 
 public interface UserMissionRepository extends JpaRepository<UserMission,Long> {
+
+    Page<UserMission> findAllByUserAndStatus(User user, MissionStatus status, PageRequest pageRequest);
 }
