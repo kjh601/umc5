4549e47431d4a826eabf45ddcc028f9daf44479d
KJH <fighter0628@gmail.com>
2023-12-03 15:28:50 +0900

    Feat(ReviewRepository): User로 review 찾는 기능 추가

--- src/main/java/umc/spring/repository/ReviewRepository.java
+++ src/main/java/umc/spring/repository/ReviewRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.spring.domain.Review;
 import umc.spring.domain.Store;
+import umc.spring.domain.User;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     Page<Review> findAllByStore(Store store, PageRequest pageRequest);
+    Page<Review> findAllByUser(User user, PageRequest pageRequest);
 }
