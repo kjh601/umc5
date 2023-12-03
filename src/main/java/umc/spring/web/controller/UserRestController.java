5a0016adebf52a6503dc5cf99e7df0bc2a88cb4e
KJH <fighter0628@gmail.com>
2023-12-03 15:27:03 +0900

    Feat(UserRestController): 특정 사용자가 작성한 리뷰 목록 조회 요청 처리

--- src/main/java/umc/spring/web/controller/UserRestController.java
+++ src/main/java/umc/spring/web/controller/UserRestController.java
@@ -1,14 +1,22 @@
 package umc.spring.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
 import umc.spring.base.apiPayload.ApiResponse;
+import umc.spring.converter.ReviewConverter;
 import umc.spring.converter.UserConverter;
+import umc.spring.domain.Review;
 import umc.spring.domain.User;
 import umc.spring.service.UserService.UserCommandService;
+import umc.spring.service.UserService.UserQueryService;
+import umc.spring.web.dto.ReviewResponseDTO;
 import umc.spring.web.dto.UserRequestDTO;
 import umc.spring.web.dto.UserResponseDTO;
 
@@ -20,10 +28,28 @@ import javax.validation.Valid;
 public class UserRestController {
 
     private final UserCommandService userCommandService;
+    private final UserQueryService userQueryService;
 
     @PostMapping("/")
     public ApiResponse<UserResponseDTO.JoinResultDTO> join(@RequestBody @Valid UserRequestDTO.JoinDto request){
         User user = userCommandService.joinUser(request);
         return ApiResponse.onSuccess(UserConverter.toJoinResultDTO(user));
+    }
+
+    @GetMapping("/{userId}/reviews")
+    @Operation(summary = "특정 유저가 작성한 리뷰 목록 조회 API", description = "특정 유저가 작성한 리뷰들의 목록을 조회하는 API이며, 페이징을 포함합니다. queryString으로 page 번호를 주세요")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003",description = "access 토큰을 주세요!", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH004",description = "access 토큰 만료", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH006",description = "access 토큰 모양이 이상함", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+    })
+    @Parameters({
+            @Parameter(name = "userId", description = "유저의 아이디, path variable 입니다!"),
+            @Parameter(name = "page", description = "페이지 번호, 0번이 1번 입니다.")
+    })
+    public ApiResponse<ReviewResponseDTO.ReviewPreviewListDTO> getReviewList(@PathVariable(name = "userId") Long userId, @RequestParam(name = "page") Integer page){
+        Page<Review> reviewPage = userQueryService.getReviewList(userId, page);
+        return ApiResponse.onSuccess(ReviewConverter.toReviewPreviewListDTO(reviewPage));
     }
 }
